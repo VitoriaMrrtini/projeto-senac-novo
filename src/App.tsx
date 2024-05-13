ea038f76247ef31fe9fb3dc1abbb4b1f00e3859e
VITORIA MARTINS ALVES <vitoria.84903@aluno.pr.senac.br>
2024-05-13 13:48:13 -0300

    Nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,9 @@ import RecipeQueijoPao from './components/siteQueijo.tsx';
 import RecipeTortaFrango from './components/siteTorta.tsx';
 import RecipePizzaCaseira from './components/sitePizza.tsx';
 import RecipeFeijoada from './components/siteFeijoada.tsx';
-<<<<<<< HEAD
 import RecipeStrogonoff from './components/siteStrogonoff.tsx';
-
-
-=======
 import RecipeSushi from './components/siteSushi.tsx';
 import RecipeLasanha from './components/siteLasanha.tsx';
->>>>>>> 0a0e6fcad8e18823e33369512d534cedf145a632
 
 function App(){
   return(
@@ -28,13 +23,9 @@ function App(){
               <Route path='/siteTorta' element={<RecipeTortaFrango/>}/>
               <Route path='/sitePizza' element={<RecipePizzaCaseira/>}/>
               <Route path='/siteFeijoada' element={<RecipeFeijoada/>}/>
-<<<<<<< HEAD
               <Route path='/siteStrogonoff' element={<RecipeStrogonoff/>}/>
-
-=======
               <Route path='/siteSushi' element={<RecipeSushi/>}/>
               <Route path='/siteLasanha' element={<RecipeLasanha/>}/>
->>>>>>> 0a0e6fcad8e18823e33369512d534cedf145a632
             </Routes>
     </BrowserRouter>
     </>
