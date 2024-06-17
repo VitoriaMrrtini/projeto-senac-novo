a39a4fb65c569b31ccfc148b76c45464aef7005a
VITORIA MARTINS ALVES <vitoria.84903@aluno.pr.senac.br>
2024-06-17 17:25:57 -0300

    Fundo

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { SCA_API_URL } from "../utils/ApiConfig";
 import CustomAlert from "./CustomAlert";
 import Swal from "sweetalert2";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 //import Modal from '../components/Modal.tsx';
 
 function Login() {
@@ -37,7 +37,7 @@ function Login() {
           Swal.fire({
             title: "Bem vindo!",
             text: "Aproveite o quanto quiser!",
-            icon: "success"
+            icon: "success",
           });
           localStorage.setItem("userID", id);
           navigate("/SiteInicio");
@@ -47,7 +47,7 @@ function Login() {
           Swal.fire({
             icon: "error",
             title: "Erro",
-            text: "Senha ou Usuário incorreto!"
+            text: "Senha ou Usuário incorreto!",
           });
         }
       })
@@ -63,35 +63,37 @@ function Login() {
   };*/
 
   return (
+    <div className="fundo">
     <div className="container">
-      <div className="login-form">
-        <h2>Login</h2>
-        <input
-          onChange={handleUserOnchange}
-          type="text"
-          name="username"
-          placeholder="Usuário"
-        />
-        <input
-          onChange={handlePasswordOnChange}
-          type="password"
-          name="password"
-          placeholder="Senha"
-        />
-        <input onClick={handleOnclick} type="submit" value="Entrar" />
-        {/*<Modal show={showModal} onClose={handleModalClose} />*/}
-        <div className="Alert">
-          {showAlert && (
-            <CustomAlert
-              message={
-                <>
-                  Não possui login? Entre <Link to="/">aqui!</Link>
-                </>
-              }
-              type="success"
-              onClose={() => setShowAlert(false)}
-            />
-          )}
+        <div className="login-form">
+          <h2>Login</h2>
+          <input
+            onChange={handleUserOnchange}
+            type="text"
+            name="username"
+            placeholder="Usuário"
+          />
+          <input
+            onChange={handlePasswordOnChange}
+            type="password"
+            name="password"
+            placeholder="Senha"
+          />
+          <input onClick={handleOnclick} type="submit" value="Entrar" />
+          {/*<Modal show={showModal} onClose={handleModalClose} />*/}
+          <div className="Alert">
+            {showAlert && (
+              <CustomAlert
+                message={
+                  <>
+                    Não possui login? Entre <Link to="/">aqui!</Link>
+                  </>
+                }
+                type="success"
+                onClose={() => setShowAlert(false)}
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
